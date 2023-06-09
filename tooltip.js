f3cfdc9a34a44dc5a4b4e61a49f51db486981f0a
Huonghuo <134469536+Huonghuo@users.noreply.github.com>
2023-06-09 14:29:47 +0700

    Update tooltip.js

--- tooltip.js
+++ tooltip.js
@@ -14,9 +14,6 @@ user.addEventListener("mouseover", show);
 user.addEventListener("mouseout", hide);
 }
 
-const togglePassword = document.querySelector("#togglePassword");
-const password = document.querySelector("#password");
-
 togglePassword.addEventListener("click", function () {
 const type = password.getAttribute("type") === "password" ? "text" : "password";
 password.setAttribute("type", type);
